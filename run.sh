512d1699f17e32afe40692814b6bd0b1bdbb3153
wxr <XingrWang@163.com>
2021-03-17 23:19:46 +0800

    minor

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=1,0,2,3 python train.py --dataset="geoimage" --batch_size=32 --test_batch_size=4 --output_dir="./visualization/geoimage" --niter=200 --net_G="./checkpoint/geoimage/netG_model_epoch_170.pth" --net_D="./checkpoint/geoimage/netD_model_epoch_170.pth"
+CUDA_VISIBLE_DEVICES=1,3 python train.py \
+    --dataset="geoimage" \
+    --batch_size=8 \
+    --test_batch_size=4 \
+    --output_dir="./visualization/geoimage" \
+    --niter=100 \
+    --net_G="./checkpoint/geoimage/netG_model_epoch_300.pth" \
+    --net_D="./checkpoint/geoimage/netD_model_epoch_300.pth"
